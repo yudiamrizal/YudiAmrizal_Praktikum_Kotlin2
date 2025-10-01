4f062a16e526fcda458977b664f2afa9812769af
yudi amrizal <yudiamrizalpnb@gmail.com>
2025-10-01 09:03:12 +0700

    Commit

--- src/Prak2_Array.kt
+++ src/Prak2_Array.kt
@@ -1,4 +1,5 @@
 fun main(args: Array<String>) {
+   /*
     //Array
     var arrA = arrayOf<Int>()
     arrA += 34
@@ -12,4 +13,10 @@ fun main(args: Array<String>) {
     for (i in arrB) {
         print("$i ")
     }
+    */
+    // Creates an Array<String> with sizes 5 and values ["0", "1", "4", "9", "16"]
+    val asc = Array(5) {
+            i -> (i * i).toString()
+    }
+    asc.forEach { println(it) }
 }
